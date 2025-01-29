ada81c2bdd86b1a3c2dbeba8bc5512a0e106f834
AnnaLena77 <anna-lena.roth@informatik-hs-fulda.de>
2025-01-29 12:00:25 +0100

    add partnerrank array for collectives

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -372,9 +372,9 @@ void qentryToBinary(qentry q, char *buffer, int *off){
             printf("Item: %s, BTL: %s, Datasize: UsedProtocol: %s\n", item->function, item->usedBtl, item->recvDatasize, item->usedAlgorithm); 
         }*/
         
-        printf("Gespeicherte Zahlen, Rank %s: ", item->processrank);
+        printf("Gespeicherte Zahlen, Rank %d: ", item->processrank);
         for (int i = 0; i <= 50; i++) {
-            if (item->coll_partnerranks[i / 8] & (1 << (i % 8))) != 0) {
+            if (item->coll_partnerranks[i / 8] & (1 << (i % 8)) != 0) {
                 printf("%d ", i);
             }
         }
