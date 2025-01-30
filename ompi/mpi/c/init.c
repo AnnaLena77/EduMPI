b67215a2d24f4b8b2d005215e155263a68c7dc03
AnnaLena77 <anna-lena.roth@informatik-hs-fulda.de>
2025-01-30 10:10:53 +0100

    bug fix for reading partnerranks

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -368,9 +368,10 @@ void qentryToBinary(qentry q, char *buffer, int *off){
         
         printf("Gespeicherte Zahlen, Rank %d: \n", item->processrank);
         for (int i = 0; i <= 50; i++) {
-            //if (item->coll_partnerranks[i / 8] & (1 << (i % 8)) != 0) {
-                printf("%d ", item->coll_partnerranks[i]);
-            //}
+            if if ((item->coll_partnerranks[i / 8] & (1 << (i % 8))) != 0)
+            {
+                printf("%d ", i);
+            }
         }
         printf("\n");
         //printf("Rank: %d, Function: %s, \n", item->processrank, item->function);
