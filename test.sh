f94941fe3b2aff9330daceb4bd179504b6e68621
AnnaLena77 <anna-lena.roth@informatik-hs-fulda.de>
2025-02-13 20:53:45 +0100

    add collective algorithm for fixed decision

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# SSH-Verbindung zum Cluster aufbauen und Commands ausführen
-ssh -T fdai0231@10.32.47.10 << EOF
-    # Kommando auf dem Cluster ausführen, z.B. sbatch
-    source ~/.bashrc        # Umgebungsvariablen laden
-    cd eduMPI_files
-    sbatch remote_bash_eduMPI.sh
-EOF
