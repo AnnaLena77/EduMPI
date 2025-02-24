f468fcc9ffe75e57bde32769731f7f84936e8402
AnnaLena77 <anna-lena.roth@informatik-hs-fulda.de>
2025-02-24 16:40:05 +0100

    test for integrating HAN

--- ompi/mca/pml/ob1/pml_ob1_isend.c
+++ ompi/mca/pml/ob1/pml_ob1_isend.c
@@ -442,11 +442,11 @@ int mca_pml_ob1_send(const void *buf,
     ompi_proc_t *dst_proc = ob1_proc->ompi_proc;
     
 #ifdef ENABLE_ANALYSIS
-    opal_proc_t *dst_opal_proc = dst_proc->super;
+    opal_proc_t *dst_opal_proc = dst_proc.super;
     opal_proc_t_name *dst_name = dst_opal_proc->opal_process_name_t;
     opal_vpid_t *dst_vpid = dst_name->vpid;
     
-    printf("Globaler Rank (vpid) in MPI_COMM_WORLD: %u\n", dst_vpid);
+    printf("Globaler Rank (vpid) in MPI_COMM_WORLD: %ls\n", dst_vpid);
 #endif
     
     mca_bml_base_endpoint_t* endpoint = mca_bml_base_get_endpoint (dst_proc);
