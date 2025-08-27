ba6c9592742eefc311477e67470c76eb686a4e38
AnnaLena77 <anna-lena.roth@informatik-hs-fulda.de>
2025-08-27 22:11:05 +0200

    fix too few arguments

--- ompi/mca/coll/self/coll_self.h
+++ ompi/mca/coll/self/coll_self.h
@@ -113,7 +113,11 @@ int mca_coll_self_alltoallw_intra(const void *sbuf, const int *scounts, const in
 #endif
                                   );
 int mca_coll_self_barrier_intra(struct ompi_communicator_t *comm,
-                                mca_coll_base_module_t *module);
+                                mca_coll_base_module_t *module
+#ifdef ENABLE_ANALYSIS
+			       , qentry **q
+#endif
+                                );
 int mca_coll_self_bcast_intra(void *buff, int count,
                               struct ompi_datatype_t *datatype,
                               int root,
